009fa07a6571765326b040610204e29525569cc9
simplyadrian <simplyadrian@gmail.com>
2015-01-20 23:27:24 -0600

    ISE-313
    used cookbook_file method to place msdeploy msi locally to node for
    install.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,15 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
+cookbook_file "WebDeploy_amd64_en-US.msi" do
+  path "C:\\chef\\"
+  source "WebDeploy_amd64_en-US.msi"
+  action :create_if_missing
+end
+
+windows_package 'Web Deploy v3.5' do
+	package_name 'Web Deploy v3.5'
+	source "C:\\chef\\WebDeploy_amd64_en-US.msi"
+	installer_type :msi
+	action :install
+end
+
 include_recipe "iis"
 include_recipe "iis-nativex::add-windows-feature"
 include_recipe "iis-nativex::create-app-pool"
 include_recipe "iis-nativex::create-config-sites"
-
-windows_package 'Web Deploy v3.5' do
-	package_name 'Web Deploy v3.5'
-	source "https://s3.amazonaws.com/installers-thirdparty-nativex/WebDeploy_amd64_en-US.msi"
-	installer_type :msi
-	options 'ADDLOCAL=ALL, /quiet -Wait'
-	action :install
-end
