e761a5a344c9b8a606240d64fae544f0e5bfc9e6
simplyadrian <simplyadrian@gmail.com>
2015-03-09 16:17:26 -0500

    corrected syntax in create-config-sites recipe

--- recipes/create-config-sites.rb
+++ recipes/create-config-sites.rb
@@ -38,7 +38,7 @@ node['iis-nativex']['enabled_sites'].each do |site_cfg|
       action [:add]
     end
   end
-  site_cfg['enabled_pools'].each do |p|
+  site_cfg['pool_name'].each do |p|
     iis_site site_cfg['pool_name'] do
       application_pool p['pool_name']
       action [:add,:start]
