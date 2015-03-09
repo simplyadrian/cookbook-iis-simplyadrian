22d63b32d75ae4ef957d8009147a068940fe2314
simplyadrian <simplyadrian@gmail.com>
2015-03-09 10:44:57 -0500

    update to create-config-sites recipe

--- recipes/create-config-sites.rb
+++ recipes/create-config-sites.rb
@@ -14,6 +14,15 @@ iis_site 'Default Web Site' do
   action [:stop, :delete]
 end
 
+#create physical locations
+node['iis-nativex']['enabled_sites'].each do |mkdir|
+  directory "C:\\inetpub\\wwwroot\\#{mkdir}" do
+    #rights :read, 'TEAMFREEZE\Everyone'
+    #rights :full_control, 'TEAMFREEZE\W3iApi'
+    action :create
+  end
+end
+
 # create and start a new site that maps to
 # the physical location C:\inetpub\wwwroot\#{site_name}
 node['iis-nativex']['enabled_sites'].each do |site_cfg|
@@ -23,25 +32,16 @@ node['iis-nativex']['enabled_sites'].each do |site_cfg|
     path site_cfg['path']
     action [:add]
   end
-  site_cfg['headers'].to_a.each do |h|
+  site_cfg['host_header'].each do |h|
     iis_site site_cfg['site_name'] do
       host_header h['host_header']
       action [:add]
     end
   end
-  site_cfg['enabled_pools'].to_a.each do |p|
+  site_cfg['enabled_pools'].each do |p|
     iis_site site_cfg['pool_name'] do
       application_pool p['pool_name']
       action [:add,:start]
     end
   end
 end
-
-#create physical locations
-#node['iis-nativex']['enabled_sites'].each do |mkdir|
-#  directory "C:\\inetpub\\wwwroot\\#{mkdir}" do
-    #rights :read, 'TEAMFREEZE\Everyone'
-    #rights :full_control, 'TEAMFREEZE\W3iApi'
-#    action :create
-#  end
-#end
