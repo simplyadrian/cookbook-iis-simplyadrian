4512a57ae776a734cb84114f6ae2eb22cd6c18d3
simplyadrian <simplyadrian@gmail.com>
2015-03-10 17:30:05 -0500

    update to create-config-site recipe

--- recipes/create-config-sites.rb
+++ recipes/create-config-sites.rb
@@ -20,31 +20,26 @@ node['iis-nativex']['enabled_sites'].each do |mkdir|
   end
 end
 
-node['iis-nativex']['enabled_sites'].each do |site_service|
-  iis_site site_service['site_name'] do
-    action :nothing
-  end
-end
-
 # create and start a new site that maps to
 # the physical location C:\inetpub\wwwroot\#{site_name}
 node['iis-nativex']['enabled_sites'].each do |site_cfg|
   iis_site site_cfg['site_name'] do
+    #bindings "#{site_cfg['protocol']/*['port']['host_header']}"
     protocol site_cfg['protocol']
     port site_cfg['port']
     path site_cfg['path']
-    action [:add, :config, :start]
+    action [:add, :config]
   end
   site_cfg['host_header'].each do |h|
     iis_site site_cfg['site_name'] do
       host_header h['host_header']
-      action [:config, :restart]
+      action [:config]
     end
   end
   site_cfg['pool_name'].each do |p|
     iis_site site_cfg['site_name'] do
       application_pool p['pool_name']
-      action [:config, :restart]
+      action [:config]
     end
   end
 end
