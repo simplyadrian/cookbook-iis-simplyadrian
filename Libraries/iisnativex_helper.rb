46e1db8ac629780f16c6a1fdbea21be8b5bac3cc
simplyadrian <simplyadrian@gmail.com>
2015-03-12 16:20:18 -0500

    added additional logging for trouble shooting

--- Libraries/iisnativex_helper.rb
+++ Libraries/iisnativex_helper.rb
@@ -16,10 +16,14 @@ module IisNativex
   module Helper
   
     def get_bindings_string( site_cfg )
+      Chef::Log.debug("Parsing site_cfg to provide values for key pairs.")
+
       bindingString = ''
+
       site_cfg[:host_header].each do |header|
       bindingString << "#{site_cfg[:protocol]}/*:#{site_cfg[:port]}:#{header}"
       end
+
     Chef::Log.debug("The string being used for each site equals #{bindingString.length}")
     return bindingString
     end
