d04099d6686ced046c6a8b117580a7caa7186e4d
simplyadrian <simplyadrian@gmail.com>
2015-03-11 19:31:31 -0500

    library testing

--- Libraries/helpers.rb
+++ Libraries/helpers.rb
@@ -8,8 +8,6 @@ module NativeX
         puts "http/*:80:#{header}"
     	bindingString.push(header)
       end
-      Chef::Log.debug("About to create #{bindingString.length}")
       bindingString
-    end
   end
 end
