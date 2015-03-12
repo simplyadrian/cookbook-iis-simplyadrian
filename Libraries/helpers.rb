c6744feca226de516626cdb32f923a46ce759336
simplyadrian <simplyadrian@gmail.com>
2015-03-11 19:22:45 -0500

    library testing

--- Libraries/helpers.rb
+++ Libraries/helpers.rb
@@ -1,8 +1,9 @@
 module NativeX
   module IIS
     module Helper
-	# We can call this with BindingsString.getBindingsString
+	# We can call this with BindingsString.getBindingsStringFromSiteConfig
     def self.getBindingsString
+      bindingString = []
       site_cfg[host_header].each do |header|
         puts "http/*:80:#{header}"
     	bindingString.push(header)
