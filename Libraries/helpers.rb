73f6b036ee4b55cb70d9ee4935d982d7923a5fd9
simplyadrian <simplyadrian@gmail.com>
2015-03-12 12:09:05 -0500

    update to helper class and usage in recipe

--- Libraries/helpers.rb
+++ Libraries/helpers.rb
@@ -3,7 +3,7 @@ module IisNativex
 
     def collection
       Chef::Log.debug("Compiling string values from site config")
-      hash = {"http/*:80:#{header}"}
+      hash = {"http/*:80:stuff","http/*:80:and","http/*:80:things"}
       array = []
       array.push(hash)
       @collection = array
