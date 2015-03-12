50277f9fb5b1b95d6c9188c60ba49e011ec83aee
simplyadrian <simplyadrian@gmail.com>
2015-03-12 17:40:37 -0500

    start sites after proper configuration has been made

--- recipes/create-config-sites.rb
+++ recipes/create-config-sites.rb
@@ -34,8 +34,6 @@ node['iis-nativex']['enabled_sites'].each do |site_cfg|
   end
   iis_site site_cfg[:site_name] do
     bindings get_bindings_string( site_cfg ).join(",")
-    action [:config]
+    action [:config, :start]
   end
 end
-
-#must be a kind of String!  You passed ["http/*:80:", "http/*:80:api.w3i.com", "http/*:80:appclick.co"].
