c1873083d5a995c4659ad04e836c7263e0bd260f
simplyadrian <simplyadrian@gmail.com>
2015-03-14 18:44:22 -0500

    ISE-313 syntax correction in install-arr-feature recipe.

--- recipes/install-arr-features.rb
+++ recipes/install-arr-features.rb
@@ -11,6 +11,7 @@
 node['iis-nativex']['services'].each do |srv|
   service srv[:services] do
   action :stop
+  end
   Chef::Log.log("Stopping IIS services before I install the ARR feature and dependencies")
 end
 
@@ -24,13 +25,14 @@ node['iis-nativex']['arr_application'].each do |win_pkg|
     timeout win_pkg[:timeout]
     version win_pkg[:version]
     action :install
-    Chef::Log.log("Installing the ARR features and dependencies.")
   end
+  Chef::Log.log("Installing the ARR features and dependencies.")
 end
 
 # Start IIS and and Web Management Service
 node['iis-nativex']['services'].each do |srv|
   service srv[:service_name] do
   action :start
+  end
   Chef::Log.log("Restarting the IIS services.")
 end
